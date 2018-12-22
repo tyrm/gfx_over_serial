77aec6bfdc599bb5026525c17a9f8c5e21677ff2
Tyr Mactire <tyr@pettingzoo.co>
2018-12-21 17:18:36 -0800

    init commit

---
+++ commands.ino
@@ -0,0 +1,115 @@
+void DrawFastHLine(String s){
+  String XStr = s.substring(2, 4);
+  String YStr = s.substring(4, 6);
+  String HStr = s.substring(6, 8);
+  String CStr = s.substring(8, 12);
+  
+  int16_t XVal = StrHexToInt(XStr);
+  int16_t YVal = StrHexToInt(YStr);
+  int16_t HVal = StrHexToInt(HStr);
+  uint16_t CVal = StrHexToInt(CStr);
+  
+  Matrix.drawFastHLine(XVal, YVal, HVal, CVal);
+}
+
+void DrawFastVLine(String s){
+  String XStr = s.substring(2, 4);
+  String YStr = s.substring(4, 6);
+  String HStr = s.substring(6, 8);
+  String CStr = s.substring(8, 12);
+  
+  int16_t XVal = StrHexToInt(XStr);
+  int16_t YVal = StrHexToInt(YStr);
+  int16_t HVal = StrHexToInt(HStr);
+  uint16_t CVal = StrHexToInt(CStr);
+  
+  Matrix.drawFastVLine(XVal, YVal, HVal, CVal);
+}
+
+void DrawLine(String s){
+  String X0Str = s.substring(2, 4);
+  String Y0Str = s.substring(4, 6);
+  String X1Str = s.substring(6, 8);
+  String Y1Str = s.substring(8, 10);
+  String CStr = s.substring(10, 14);
+  
+  int16_t X0Val = StrHexToInt(X0Str);
+  int16_t Y0Val = StrHexToInt(Y0Str);
+  int16_t X1Val = StrHexToInt(X1Str);
+  int16_t Y1Val = StrHexToInt(Y1Str);
+  uint16_t CVal = StrHexToInt(CStr);
+  
+  Matrix.drawLine(X0Val, Y0Val, X1Val, Y1Val, CVal);
+}
+
+void DrawPixel(String s){
+  String XStr = s.substring(2, 4);
+  String YStr = s.substring(4, 6);
+  String CStr = s.substring(6, 10);
+  
+  int16_t XVal = StrHexToInt(XStr);
+  int16_t YVal = StrHexToInt(YStr);
+  uint16_t CVal = StrHexToInt(CStr);
+  
+  Matrix.drawPixel(XVal, YVal, CVal);
+}
+
+void DrawRect(String s){
+  String XStr = s.substring(2, 4);
+  String YStr = s.substring(4, 6);
+  String WStr = s.substring(6, 8);
+  String HStr = s.substring(8, 10);
+  String CStr = s.substring(10, 14);
+  
+  int16_t XVal = StrHexToInt(XStr);
+  int16_t YVal = StrHexToInt(YStr);
+  int16_t WVal = StrHexToInt(WStr);
+  int16_t HVal = StrHexToInt(HStr);
+  uint16_t CVal = StrHexToInt(CStr);
+  
+  Matrix.drawRect(XVal, YVal, WVal, HVal, CVal);
+}
+
+void FillRect(String s){
+  String XStr = s.substring(2, 4);
+  String YStr = s.substring(4, 6);
+  String WStr = s.substring(6, 8);
+  String HStr = s.substring(8, 10);
+  String CStr = s.substring(10, 14);
+  
+  int16_t XVal = StrHexToInt(XStr);
+  int16_t YVal = StrHexToInt(YStr);
+  int16_t WVal = StrHexToInt(WStr);
+  int16_t HVal = StrHexToInt(HStr);
+  uint16_t CVal = StrHexToInt(CStr);
+  
+  Matrix.fillRect(XVal, YVal, WVal, HVal, CVal);
+}
+
+void FillScreen(String s){
+  String CStr = s.substring(2, 6);
+  uint16_t CVal = StrHexToInt(CStr);
+  
+  Matrix.fillScreen(CVal);
+}
+
+void InvertDisplay(String s){
+  String IStr = s.substring(2, 4);
+  int16_t IVal = StrHexToInt(IStr);
+
+  if (IVal == 0) {
+    Serial.println("Uninverting");
+    Matrix.invertDisplay(false);
+  }
+  else {
+    Serial.println("Inverting");
+    Matrix.invertDisplay(true);
+  }
+}
+
+void SetRotation(String s){
+  String RStr = s.substring(2, 4);
+  int16_t RVal = StrHexToInt(RStr);
+  
+  Matrix.setRotation(RVal);
+}
---
+++ gfx_over_serial.ino
@@ -0,0 +1,97 @@
+#include <RGBmatrixPanel.h>
+
+#define CLK  13
+#define OE   1  // TX
+#define LAT  0  // RX
+#define A   A5
+#define B   A4
+#define C   A3
+#define D   A2
+
+// the RGB data pins on featherwing, must be on same PORT as CLK
+uint8_t rgbpins[] = { 6,5,9,11,10,12 };
+RGBmatrixPanel Matrix(A, B, C, D, CLK, LAT, OE, false, 64, rgbpins);
+
+// serial buffer 
+String sBuffer = "";
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {}
+  
+  sBuffer.reserve(255);
+  
+  Matrix.begin();
+  Matrix.setRotation(1);
+
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char inChar = (char)Serial.read();
+    
+    if (inChar == 13 || inChar == 10) {
+      ProcessBuffer();
+    }
+    else {
+      sBuffer += inChar;
+    }
+  }
+}
+
+String FormatHex8(uint8_t data) {
+  String value = "";
+  if (data<0x10) {value = value + "0";} 
+  value = value + String(data, HEX);
+
+  return value;
+}
+
+void ProcessBuffer() {
+  String command = sBuffer.substring(0, 2);
+  int commandVal = StrHexToInt(command);
+
+  switch(commandVal) {
+    case 0x01:
+      DrawPixel(sBuffer);
+      break;
+    case 0x02:
+      SetRotation(sBuffer);
+      break;
+    case 0x03:
+      InvertDisplay(sBuffer);
+      break;
+    case 0x04:
+      DrawFastVLine(sBuffer);
+      break;
+    case 0x05:
+      DrawFastHLine(sBuffer);
+      break;
+    case 0x06:
+      FillRect(sBuffer);
+      break;
+    case 0x07:
+      FillScreen(sBuffer);
+      break;
+    case 0x08:
+      DrawLine(sBuffer);
+      break;
+    case 0x09:
+      DrawRect(sBuffer);
+      break;
+    default:
+      // 0xfe: I don't understand
+      Serial.println("fe");
+      break;
+  }
+
+  sBuffer = "";
+}
+
+int StrHexToInt(String str) {
+  int bufferSize = sizeof(str) + 1;
+  char strBuffer[bufferSize];
+  str.toCharArray(strBuffer, bufferSize);
+  
+  return (int) strtol(strBuffer, 0, 16);
+}
